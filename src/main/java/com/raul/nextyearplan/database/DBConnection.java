9d1cdb6f0cb2397225578cb9b6eafd995b57d6f9
RaulC <raul.carricoba.g@gmail.com>
2022-08-28 08:37:10 +0200

    Added new tables and fixed connection issues

--- src/main/java/com/raul/nextyearplan/database/DBConnection.java
+++ src/main/java/com/raul/nextyearplan/database/DBConnection.java
@@ -18,43 +18,55 @@ public class DBConnection {
 
             conn = DriverManager.getConnection(Constants.DB_URL, Constants.USER, Constants.PASS);
             ps = conn.createStatement();
-            final String sql = "CREATE TABLE IF NOT EXISTS STUDENT " +
-                    "(studentId INTEGER not NULL, " +
-                    " studentCode VARCHAR(255), " +
-                    " fileNumber INTEGER, " +
-                    " fullName VARCHAR(255), " +
-                    " age INTEGER, " +
-                    " PRIMARY KEY ( studentId ));" +
+            final String sql =
 
-                    "CREATE TABLE IF NOT EXISTS SUBJECT " +
-                    "(subjectId INTEGER not NULL, " +
-                    " subjectName VARCHAR(255), " +
-                    " subjectCode VARCHAR(255), " +
-                    " optional BOOLEAN, " +
-                    " PRIMARY KEY ( subjectId ));" +
+                    "CREATE TABLE IF NOT EXISTS GRADE " +
+                            "(gradeId INTEGER not NULL, " +
+                            " name VARCHAR(255), " +
+                            " gradeCode INTEGER, " +
+                            " PRIMARY KEY ( gradeId ));" +
 
-                    "CREATE TABLE IF NOT EXISTS TEACHER " +
-                    "(teacherId INTEGER not NULL, " +
-                    " fullName VARCHAR(255), " +
-                    " age INTEGER, " +
-                    " permanent BOOLEAN, " +
-                    " PRIMARY KEY ( teacherId ));" +
+                            "CREATE TABLE IF NOT EXISTS STUDENT " +
+                            "(studentId INTEGER not NULL, " +
+                            " gradeId INTEGER not NULL, " +
+                            " studentCode VARCHAR(255), " +
+                            " fileNumber INTEGER, " +
+                            " fullName VARCHAR(255), " +
+                            " age INTEGER, " +
+                            " PRIMARY KEY ( studentId ), " +
+                            " FOREIGN KEY (gradeId) REFERENCES GRADE(gradeId));" +
 
-                    "CREATE TABLE IF NOT EXISTS STUDENT_SUBJECT" +
-                    "(studentSubjectId INTEGER not NULL, " +
-                    " studentId INTEGER not NULL, " +
-                    " subjectId INTEGER not NULL, " +
-                    " PRIMARY KEY ( studentSubjectId ), " +
-                    " FOREIGN KEY (studentId) REFERENCES STUDENT(studentId), " +
-                    " FOREIGN KEY (subjectId) REFERENCES SUBJECT(subjectId));" +
+                            "CREATE TABLE IF NOT EXISTS SUBJECT " +
+                            "(subjectId INTEGER not NULL, " +
+                            " gradeId INTEGER not NULL, " +
+                            " subjectName VARCHAR(255), " +
+                            " subjectCode VARCHAR(255), " +
+                            " optional BOOLEAN, " +
+                            " PRIMARY KEY ( subjectId ), " +
+                            " FOREIGN KEY (gradeId) REFERENCES GRADE(gradeId));" +
 
-                    "CREATE TABLE IF NOT EXISTS TEACHER_SUBJECT" +
-                    "(teacherSubjectId INTEGER not NULL, " +
-                    " teacherId INTEGER not NULL, " +
-                    " subjectId INTEGER not NULL, " +
-                    " PRIMARY KEY ( teacherSubjectId ), " +
-                    " FOREIGN KEY (teacherId) REFERENCES TEACHER(teacherId), " +
-                    " FOREIGN KEY (subjectId) REFERENCES SUBJECT(subjectId));";
+                            "CREATE TABLE IF NOT EXISTS TEACHER " +
+                            "(teacherId INTEGER not NULL, " +
+                            " fullName VARCHAR(255), " +
+                            " age INTEGER, " +
+                            " permanent BOOLEAN, " +
+                            " PRIMARY KEY ( teacherId ));" +
+
+                            "CREATE TABLE IF NOT EXISTS STUDENT_SUBJECT" +
+                            "(studentSubjectId INTEGER not NULL, " +
+                            " studentId INTEGER not NULL, " +
+                            " subjectId INTEGER not NULL, " +
+                            " PRIMARY KEY ( studentSubjectId ), " +
+                            " FOREIGN KEY (studentId) REFERENCES STUDENT(studentId), " +
+                            " FOREIGN KEY (subjectId) REFERENCES SUBJECT(subjectId));" +
+
+                            "CREATE TABLE IF NOT EXISTS TEACHER_SUBJECT" +
+                            "(teacherSubjectId INTEGER not NULL, " +
+                            " teacherId INTEGER not NULL, " +
+                            " subjectId INTEGER not NULL, " +
+                            " PRIMARY KEY ( teacherSubjectId ), " +
+                            " FOREIGN KEY (teacherId) REFERENCES TEACHER(teacherId), " +
+                            " FOREIGN KEY (subjectId) REFERENCES SUBJECT(subjectId));";
 
             ps.executeUpdate(sql);
 
@@ -64,11 +76,15 @@ public class DBConnection {
             e.printStackTrace();
         } finally {
             try {
-                ps.close();
+                if (ps != null) {
+                    ps.close();
+                }
             } catch (final Exception e) {
             }
             try {
-                conn.close();
+                if (conn != null) {
+                    conn.close();
+                }
             } catch (final Exception e) {
             }
         }
